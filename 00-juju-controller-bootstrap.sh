ce681003ed0ae6de95fe0003a5725edbd02953f8
fenar <fenar@yahoo.com>
2017-06-14 08:58:51 -0500

    Updates

--- 00-juju-controller-bootstrap.sh
+++ 00-juju-controller-bootstrap.sh
@@ -6,5 +6,5 @@ set -eaux
 obnum=`hostname | cut -c 10- -`
 
 time juju bootstrap --config bootstrap-timeout=1200 --to node00vm1ob${obnum}.maas --show-log v4n${obnum}-maas  maas-v4n${obnum}-controller
-
+echo "..."
 juju gui --no-browser --show-credentials
