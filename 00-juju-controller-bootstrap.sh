3979bde196ede4f1e2a6f05b6a3e2e7632ae8e08
fenar@yahoo.com <fenar@yahoo.com>
2017-06-14 07:43:49 -0500

    Updates

--- 00-juju-controller-bootstrap.sh
+++ 00-juju-controller-bootstrap.sh
@@ -5,6 +5,6 @@ set -eaux
 
 obnum=`hostname | cut -c 10- -`
 
-time juju bootstrap --config bootstrap-timeout=1200 --to node00vm0ob${obnum}.maas --show-log maas maas/172.27.${obnum}.1
+time juju bootstrap --config bootstrap-timeout=1200 --to node00vm1ob${obnum}.maas --show-log v4n${obnum}-maas  maas-v4n${obnum}-controller
 
 juju gui --no-browser --show-credentials
