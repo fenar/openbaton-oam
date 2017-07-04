d9c40b860b01dce4d8ceabf910b5f007869a8353
fenar@yahoo.com <fenar@yahoo.com>
2017-07-04 17:20:23 -0500

    Update to Service Restart

--- service-restart.sh
+++ service-restart.sh
@@ -16,11 +16,22 @@ print '\n'.join(services["$1"]['units'].keys())
 EOF
 }
 
+do_restart()
+{
+cat << EOF| python -
+import json, subprocess, re
+from subprocess import check_output
+
+cmd = ["juju", "ssh", "$1", "sudo reboot now"]
+out = check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+EOF
+}
+
 for service in ${openstack_service_list[@]}
 do
     for unit in `get_units $service`
     do
-        juju ssh $unit "sudo reboot now"
+        do_restart $unit
         echo -e "INFO-LOG:$(date): Reboot-ing $unit"
     done
 done
