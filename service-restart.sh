97aa2e907009f9d32dcf956d72fb638d747f9616
fenar <fenar@yahoo.com>
2017-07-06 14:16:45 -0500

    Updates to bundle

--- service-restart.sh
+++ service-restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Trigger a service restart
 #
-openstack_service_list=(openstack-dashboard keystone glance cinder neutron-api nova-cloud-controller neutron-gateway)
+openstack_service_list=(openstack-dashboard keystone glance cinder neutron-api nova-cloud-controller neutron-gateway openbaton)
 
 for service in ${openstack_service_list[@]}
 do
