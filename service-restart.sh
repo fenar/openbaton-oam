25ef7a586077b0294c9c1d15a18efda2899a3b0a
fenar <fenar@yahoo.com>
2017-07-06 14:11:34 -0500

    Updates for Service Restart

--- service-restart.sh
+++ service-restart.sh
@@ -1,37 +1,9 @@
-#!/bin/bash -eu
+#!/bin/bash
 # Trigger a service restart
 #
 openstack_service_list=(openstack-dashboard keystone glance cinder neutron-api nova-cloud-controller neutron-gateway)
 
-get_units ()
-{
-cat << EOF| python -
-import json, subprocess, re
-from subprocess import check_output
-
-data = subprocess.check_output(['juju', 'status', '--format=json'])
-j = json.loads(data)
-services = j['applications']
-print '\n'.join(services["$1"]['units'].keys())
-EOF
-}
-
-do_restart()
-{
-cat << EOF| python -
-import json, subprocess, re
-from subprocess import check_output
-
-cmd = ["juju", "ssh", "$1", "\"sudo reboot now\""]
-out = check_output(cmd, shell=True)
-EOF
-}
-
 for service in ${openstack_service_list[@]}
 do
-    for unit in `get_units $service`
-    do
-        do_restart $unit
-        echo -e "INFO-LOG:$(date): Reboot-ing $unit"
-    done
+  juju ssh $service/0 "sudo reboot now"
 done
